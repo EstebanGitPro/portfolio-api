f2bae4762e10cc1ee39a1485b6881741a8b03911
EstebanGitPro <estebanpoly.e.a@gmail.com>
2026-06-30 10:53:33 -0500

    feat(api): add CRUD stubs, rename mapper method to toProjectResponseDTO

--- src/main/java/com/estebangitpro/portfolio/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/estebangitpro/portfolio/service/impl/ProjectServiceImpl.java
@@ -3,6 +3,7 @@ package com.estebangitpro.portfolio.service.impl;
 import com.estebangitpro.portfolio.dto.ProjectRequestDTO;
 import com.estebangitpro.portfolio.dto.ProjectResponseDTO;
 import com.estebangitpro.portfolio.mapper.ProjectMapper;
+import com.estebangitpro.portfolio.model.Project;
 import com.estebangitpro.portfolio.repository.ProjectRepository;
 import com.estebangitpro.portfolio.service.ProjectService;
 import lombok.RequiredArgsConstructor;
@@ -19,14 +20,41 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public ProjectResponseDTO createProject(ProjectRequestDTO requestDTO) {
-        return null;
+
+       Project project = mapper.toProject(requestDTO);
+       repository.save(project);
+        return mapper.toProjectResponseDTO(project);
     }
 
     @Override
     public List<ProjectResponseDTO> getAllProjects() {
         return repository.findByPublishedTrueOrderByOrderAsc()
                 .stream()
-                .map(mapper::toResponseDTO)
+                .map(mapper::toProjectResponseDTO)
                 .toList();
     }
+
+
+    @Override
+    public ProjectResponseDTO getProductById(String id) {
+        Project project = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Producto no encontrado con el id : " +  id)
+        );
+        return mapper.toProjectResponseDTO(project);
+    }
+
+    @Override
+    public ProjectResponseDTO updateProject(String id, ProjectRequestDTO requestDTO) {
+        Project project = repository.findById(id).orElseThrow();
+        ProjectResponseDTO projectResponseDTO = mapper.toProjectResponseDTO(project);
+
+        return null;
+    }
+
+    @Override
+    public void deleteProject(String id) {
+        Project project = repository.findById(id).orElseThrow();
+        repository.delete(project);
+    }
+
 }
